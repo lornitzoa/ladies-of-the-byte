5b04a6bd0465153c3c09fed18205952682f81d7f
Cori Caridine <caridine.corin@gmail.com>
2019-02-08 17:28:12 -0600

    iniital set up for authorization

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const session = require('express-session');
 
-app.use(express.json()); 
+app.use(express.json());
+app.use(express.static("public"));
 
 const planController = require('./controllers/plan.js');
 app.use('/plan/', planController);
